52ecdaa516b327d5f3d242e0f392158c970aae29
Yuriy <fearussia86@gmail.com>
2019-04-11 16:00:00 +0400

    Коммит от 11.04.2019 с ДЗ по JS #6

---
+++ hometask-lesson6/js/hometask6.js
@@ -0,0 +1,87 @@
+
+
+(function () {
+
+'use strict';
+
+
+let articles = [
+  {
+      id: "#",
+      title: "Title",
+      description: "description",
+      author: "author"
+  },
+
+    {
+        id: 1,
+        title: "JS",
+        description: "Описание",
+        author: "Mike"
+    },
+    {
+        id: 2,
+        title: "PHP",
+        description: "Описание",
+        author: "Mike"
+    },
+    {
+        id: 3,
+        title: "HTML",
+        description: "Описание",
+        author: "Alex"
+    },
+    {
+        id: 4,
+        title: "Базы Данных",
+        description: "Описание",
+        author: "Peter"
+    }
+];
+
+
+function ourTable(array){
+  let table = document.createElement("table");
+  table.setAttribute("border", "1");
+  let caption = table.createCaption();
+  caption.setAttribute("style", "color: red", "font-weight: bold"); //Почему не работают два атрибута одновременно?
+  caption.setAttribute("style", "font-weight: bold"); //Почему не работают два атрибута одновременно?
+
+  caption.innerText = "Caption";
+
+  let row = table.insertRow(0);
+
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  let cell4 = row.insertCell(3);
+
+cell1.innerText="id";
+cell2.innerText="title";
+cell3.innerText="description";
+cell4.innerText="author";
+
+for (let i=1; i<array.length; i++) {
+  let rowtable = table.insertRow(); //нужны ли вообще скобки? insertRow() - это такая конструкция кода?
+
+  for (let key in array[i]) {
+      let cell = rowtable.insertCell();
+      cell.innerText = array[i][key];
+  }
+
+}
+
+
+
+
+let divTable = document.getElementById("table");
+divTable.appendChild(table);
+}
+
+
+
+
+
+ourTable(articles);
+
+} () );
